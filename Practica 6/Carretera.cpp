e02ce4d919a39a518db8e1d2b47eda840d0f24a5
Josep-Win <jodoldar@gmail.com>
2017-11-29 20:28:49 +0100

    Mierda6

--- Practica 6/Carretera.cpp
+++ Practica 6/Carretera.cpp
@@ -9,6 +9,8 @@ Autor: Josep Dols
 #define tasaFPS 60
 #define Ancho 20
 #define Alto 10
+#define AMPLITUD 100
+#define PERIODO 1000
 
 #include <iostream>
 #include <sstream>
@@ -19,8 +21,21 @@ Autor: Josep Dols
 using namespace std;
 
 GLfloat v0[3] = { 0,0,5 }, v1[3] = { 20,0,5 }, v2[3] = { 20,0,-5 }, v3[3] = { 0,0,-5 };
-
 GLfloat v0Ini[3] = { 0,0,-5 }, v1Ini[3] = { 0,0,-15 }, v2Ini[3] = { 20,0,-15 }, v3Ini[3] = { 20,0,-5 };
+
+float vecNorm[2] = { 0,0 };
+float vecVel[3] = { 0,0,0 };
+
+float velocidad, difVelocidad, angulo, difAngulo;
+float xCam, xView;
+float zCam, zView;
+
+float xCarr,zCarr;
+
+float derivadaDe(float u)
+{
+	return ((2 * PI*AMPLITUD) / PERIODO)*cos(u*((2 * PI) / PERIODO));
+}
 
 void init()
 {
@@ -29,7 +44,14 @@ void init()
 
 	glEnable(GL_DEPTH_TEST);
 
-	
+	velocidad = 0; difVelocidad = 0;
+	angulo = 0, difAngulo = 0;
+	xCam = -20;
+	zCam = 0;
+	xView = 25;
+	zView = 0;
+	zCarr = 0;
+	xCarr = 0;
 
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 }
@@ -51,7 +73,7 @@ void muestraFPS()
 		// Modificar la barra de titulos
 		stringstream titulo;
 		titulo << "Practica 6 - FPS = " << fotogramas;
-		glutSetWindowTitle(titulo.str().c_str());
+		//glutSetWindowTitle(titulo.str().c_str());
 		// Reinicio la cuenta de fotogramas
 		fotogramas = 0;
 		antes = ahora;
@@ -66,24 +88,57 @@ void display()
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	gluLookAt(10, 10, -20, 10, 0, 10, 0, 0, 1);
+	velocidad += difVelocidad;
+	if (velocidad < 0) { velocidad = 0; }
+
+	angulo += difAngulo;
+
+	vecVel[0] = abs(cos(angulo));
+	vecVel[2] = abs(sin(angulo));
+
+	//printf("Angulo: %f Vector 0: %f, Vector 1: %f, Vector 2: %f\n",angulo, vecVel[0], vecVel[1], vecVel[2]);
+	xCam += velocidad;
+	printf("Derivada: %f\n", derivadaDe(xCam));
+
+	
+
+
+	zView = (10 * sin(angulo)) / sin(90 - angulo);
+	gluLookAt(xCam, 10, 0, xView + xCam, 0, 0, 0, 1, 0);
 
 	ejes();
-
+	glRotatef(90, 0, 1, 0);
+	glTranslatef(-10, 0, 0);
 	glColor3f(0.0f, 0.0f, 0.0f);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	glColor3f(0.0f, 0.0f, 0.0f);
-	quad(v0Ini, v1Ini, v2Ini, v3Ini);
-	for (int i = 0; i <= 20; i++) {
-		GLfloat v0Aux[3] = { 0, 0, 5 + i*Alto };
-		GLfloat v1Aux[3] = { 20, 0, 5 + i*Alto };
-		GLfloat v2Aux[3] = { 20, 0, -5 + i*Alto };
-		GLfloat v3Aux[3] = { 0, 0, -5 + i*Alto };
-		quad(v0Aux, v3Aux, v2Aux, v1Aux);
+	//quad(v0Ini, v1Ini, v2Ini, v3Ini);
+	for (int i = -1; i <= 20; i++) {
+		vecNorm[0] = (1 / sqrt(1 + pow(derivadaDe(xCam), 2)))*derivadaDe(xCam)*-1;
+		vecNorm[1] = (1 / sqrt(1 + pow(derivadaDe(xCam), 2)));
+
+		zCarr = AMPLITUD * sin(xCam*((2 * PI) / PERIODO));
+		xCarr = i*Alto;
+
+		GLfloat v0Aux[3] = { xCarr - vecNorm[0]*Ancho/2, 0, zCarr - vecNorm[1]*Ancho/2};
+		GLfloat v3Aux[3] = { xCarr + vecNorm[0] * Ancho / 2, 0, zCarr + vecNorm[1] * Ancho / 2 };
+
+		vecNorm[0] = (1 / sqrt(1 + pow(derivadaDe(xCam+20), 2)))*derivadaDe(xCam+20)*-1;
+		vecNorm[1] = (1 / sqrt(1 + pow(derivadaDe(xCam+20), 2)));
+
+		zCarr = AMPLITUD * sin((20+xCam)*((2 * PI) / PERIODO));
+		xCarr = 20 + i*Alto;
+
+		GLfloat v1Aux[3] = { xCarr - vecNorm[0] * Ancho / 2, 0, zCarr - vecNorm[1] * Ancho / 2 };
+		GLfloat v2Aux[3] = { xCarr + vecNorm[0] * Ancho / 2, 0, zCarr + vecNorm[1] * Ancho / 2 };
+		
+		quad(v0Aux, v1Aux, v2Aux, v3Aux);
 	}
 
-	glFlush();
-	//glutSwapBuffers();
+
+	difVelocidad = 0; difAngulo = 0;
+	//glFlush();
+	glutSwapBuffers();
 }
 
 void onReshape(GLint w, GLint h)
@@ -144,10 +199,52 @@ void onTimer(int tiempo)
 	update();
 }
 
+void onKey(unsigned char tecla, int x, int y) 
+{
+	stringstream titulo;
+	titulo << "Apretada tecla " << tecla;
+	glutSetWindowTitle(titulo.str().c_str());
+
+	glutPostRedisplay();
+}
+
+void onEspecialKey(int specialKey, int x, int y)
+{
+	stringstream titulo;
+	switch (specialKey)
+	{
+		case GLUT_KEY_LEFT:
+			difAngulo = -0.25;
+			titulo << "Tecla: FLECHA IZQ.";
+			glutSetWindowTitle(titulo.str().c_str());
+			break;
+		case GLUT_KEY_RIGHT:
+			difAngulo = 0.25;
+			titulo << "Tecla: FLECHA DER.";
+			glutSetWindowTitle(titulo.str().c_str());
+			break;
+		case GLUT_KEY_UP:
+			difVelocidad = 0.1;
+			titulo << "Tecla: FLECHA ARRIBA";
+			glutSetWindowTitle(titulo.str().c_str());
+			break;
+		case GLUT_KEY_DOWN:
+			difVelocidad = -0.1;
+			titulo << "Tecla: FLECHA ABAJO";
+			glutSetWindowTitle(titulo.str().c_str());
+			break;
+		default:
+			break;
+	}
+
+	printf("Velocidad %f, Angulo %f\n", velocidad, angulo);
+	glutPostRedisplay();
+}
+
 void main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH); //GLUT_DOUBLE
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH); //GLUT_DOUBLE
 	glutInitWindowSize(400, 400);
 	glutInitWindowPosition(50, 200);
 	glutCreateWindow(PROYECTO);
@@ -155,7 +252,11 @@ void main(int argc, char** argv)
 	glutDisplayFunc(display);
 	glutReshapeFunc(onReshape);
 	//glutIdleFunc(update);
-	//glutTimerFunc(1000 / tasaFPS, onTimer, 1000 / tasaFPS);
+	
+	glutKeyboardFunc(onKey);
+	glutSpecialFunc(onEspecialKey);
+
+	glutTimerFunc(1000 / tasaFPS, onTimer, 1000 / tasaFPS);
 
 	glutMainLoop();
 }
